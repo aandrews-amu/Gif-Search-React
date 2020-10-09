d5ab56a092b913fb439aeb52b31f97737efd4ab4
aandrews-amu <aandrews@amuniversal.com>
2020-10-09 14:55:46 -0500

    Notes on querySelectors and eventListeners

--- dist/index.js
+++ dist/index.js
@@ -189,11 +189,56 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
+},{"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"boxes.js":[function(require,module,exports) {
+// ------------- QUERY SELECTORS ---------------
+// QUERY SELECTORS ARE THE BETTER WAY TO SELECT ELEMENTS
+// YOU NEED TO INCLUDE "." FOR CLASS and "#" FOR ID IN THE SELECTION
+// GET FIRST INSTANCE OF CLASS SQUARE
+// var square = document.querySelector(".square");
+// console.log(square);
+// GET ALL INSTANCES OF CLASS SQUARE (in a NodeList)
+// var squares = document.querySelectorAll(".square");
+// console.log(squares);
+// PARENT CHILD SELECTORS 
+// var squareAsChild = document.querySelectorAll(".boxes__box .square");
+// console.log(squareAsChild); 
+// ------------- GET ELEMENT SELECTORS --------------- 
+// THE FOLLOWING ARE OLDER FUNCTIONS USED BEFORE QUERY SELECTORS CAME ABOUT
+// THEY ARE DIFFICULT TO USE ACCORDING TO WES
+// YOU DONT PUT ".class" or "#id" JUST PASS "class" or "id"
+// .getElementByClassName("class") 
+// .getElementByTagName("tag") 
+// .getElementById("id")
+// ------------- ADD AND REMOVE CLASSES ----------------
+// var firstGif = document.querySelector(".gifs__gif .img-fluid"); 
+// console.log(firstGif);
+// firstGif.classList.add('test');
+// firstGif.classList.remove('test');
+// TOGGLE CLASS WITH EVENT LISTENER
+// function toggleRound() {
+//   firstGif.classList.toggle('round');
+// }
+// firstGif.addEventListener('click', toggleRound);
+// -------- EVENT LISTEN ON MULTIPLE ITEMS --------------
+// MUST USE FOR EACH LOOP
+// var squares = document.querySelectorAll(".square");
+// squares.forEach(function(square) {
+//   square.classList.add('round')
+// });
+//------------- REFACTOR OF WHAT WE WERE WORKING ON ----------------
+// var el = document.querySelectorAll(".square");
+// el.forEach(sq => {
+//   sq.addEventListener('click', function() {
+//     sq.style.height = "100px";
+//   })
+// })
+},{}],"index.js":[function(require,module,exports) {
 "use strict";
 
 require("./style.scss");
-},{"./style.scss":"style.scss"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+
+require('./boxes.js');
+},{"./style.scss":"style.scss","./boxes.js":"boxes.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -221,7 +266,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53463" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64854" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- index.js
+++ index.js
@@ -1,1 +1,2 @@
 import "./style.scss";
+require('./boxes.js'); 
